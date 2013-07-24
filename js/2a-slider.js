6fb5344ff6f105b3925033fbd5ec15c532eb4a8c
Nathanael Smith <git@natesmith.name>
2013-07-24 16:43:41 -0700

    cleaned up code a bit, using literal object more effectively now.

--- js/2a-slider.js
+++ js/2a-slider.js
@@ -2,7 +2,7 @@
 	
 	var methods = {
 		init : function ( options ) {
-			var settings = $.extend({
+			var s, settings = $.extend({
 				container : this,
 				slideHolder : "ul",
 				slide : "li",
@@ -21,7 +21,7 @@
 				},
 				isAnimating : 0,
 				speed : 200,
-				auto : false,
+				auto : true,
 				delay : 6000,
 				nav : true,
 				pause : true,
@@ -29,177 +29,121 @@
 				carousel : false,
 				loop : false
 			}, options);
-			$(this).data('s', settings);
-			var s = $(this).data('s');
+			s = methods.s = settings;
 						
 			// Setup carousel mode
-			if (s.carousel == true) {
+			if (s.carousel) {
 				s.container.find(s.slideHolder).width((s.slideCount() * s.slideWidth()) + 'px');
 			}
-			// Setup progress bar
-			if (s.progress) {
-				s.auto = true;
-			}
-
-			this.tinySlider('navCheck');
+			methods.navCheck();
+			methods.bindEvents();
 			
 			// Set active states
 			s.container.find(s.slideHolder + ' > ' + s.slide + ":first-child").addClass('active');
 			s.container.find(s.slideNav + ' ' + s.navBtns.slide + ":first-child").addClass('active');
-			t=setTimeout(function(){
-				s.container.trigger('activeSlide');
-			},500);
-			
-			// Bind navigation
-			s.container.find(s.navBtns.right).click(function(e){
-				if(!s.isAnimating) {
-					e.preventDefault();
-					if (s.carousel == true) {
-						shiftCarousel('right');
-					} else {
-						nextSlide('right');
-					}
-					if (s.auto) {
-						clearInterval(auto);
-						autoRotate();
-					}
-				}
-			});
-			s.container.find(s.navBtns.left).click(function(e){
-				if(!s.isAnimating) {
-					e.preventDefault();
-					if (s.carousel == true) {
-						shiftCarousel('left');
-					} else {
-						nextSlide('left');
-					}
-					if (s.auto) {
-						clearInterval(auto);
-						autoRotate();
-					}
-				}
-			});
-			s.container.find(s.slideNav).children().click(function(e){
-				if(!s.isAnimating && !$(this).hasClass('active')) {
-					e.preventDefault();
-					nextSlide(parseInt($(this).attr('data-slide-id')));
-				}
-			});
-			
+						
 			// Start auto rotate
-			$(function(){
-				if (s.auto) {
-					autoRotate();
-					if (s.pause) {
-		  				s.container.find(s.slide).hover(function(){ 
-		  					autoRotate('pause');
-		  					if (s.progress) {
-		  						progress('pause');
-		  					}
-		  				}, function() {
-		  					autoRotate();
-		  				});
-					}
-				}			
-			});
-			var autoRotate = function (action) {
-				if(action == 'pause') {
-					clearInterval(auto);
-				} else {
-					auto = window.setInterval( function() {
-						if (s.carousel) {
-							shiftCarousel('right');
-						} else {
-							nextSlide('right');
-						}
-					}, s.delay);
-		  			if (s.progress) {
-						progress();
-					}
-				}
-			}
-			
-			var nextSlide = function (direction) {
-				var currentSlide = s.container.find(s.slideHolder + ' > ' + s.slide + '.active').index(), nextSlide, animateFrom, animateTo;
-				s.isAnimating = 1;
-				if(typeof direction === 'number') {
-					nextSlide = direction;
-					direction = (nextSlide > currentSlide) ? 'right' : 'left';
-				}
-				if (direction == 'right') {
-					if (typeof nextSlide !== 'number') {
-						nextSlide = (currentSlide == (s.slideCount() - 1)) ? 0 : currentSlide + 1;
-					}
-					animateFrom = s.slideWidth();
-					animateTo = '-=' + s.slideWidth();
-				} else {
-					if (typeof nextSlide !== 'number') {
-						nextSlide = (currentSlide == 0) ? s.slideCount() - 1 : currentSlide - 1;
-					}
-					animateFrom = -s.slideWidth();
-					animateTo = '+=' + s.slideWidth();
-				}
-				// Animate next slide
-				s.container.find(s.slideHolder + ' > ' + s.slide +  ':eq(' + nextSlide + ')')
-				  .css('left',animateFrom)
-				  .show()
-				  .animate({
-				  		left: animateTo
-				    }, s.speed, 
-				    function() {
-					    $(this).addClass('active');
-					    s.isAnimating = 0;
-					    s.container.find(s.slideNav + ' ' + s.navBtns.slide + ":eq(" + nextSlide + ")").addClass('active').siblings().removeClass('active');
-				  });
-				// Animate active slide
-				s.container.find(s.slideHolder + ' > ' + s.slide + '.active').animate({
-				    left: animateTo
-				  }, s.speed, function() {
-				    $(this).removeClass('active').hide();
-					s.container.trigger('activeSlide');
+			if (s.auto) {
+				$(function(){
+					methods.autoRotate('start');
 				});
-		  		if (s.progress) {
-					$('.progress .bar').stop();
-			  		progress();
-				}
-			}
-			
-			var shiftCarousel = function (direction) {
-				var cw = s.slideCount() * s.slideWidth(),
-					co = Math.abs(parseFloat(s.container.find(s.slideHolder).css('left'))),
-					cv = parseFloat(s.container.width()),
-					cp = parseFloat(s.container.css('paddingLeft'));
-				if((s.container.find(s.slideHolder + ':animated').length == 0) && 
-				((direction == 'left' && co != cp) || 
-				(direction == 'right' && (co + cv + cp) < cw))) {
-					s.container.find(s.slideHolder).animate({
-						left: ((direction == 'left') ? '+=' : '-=') + s.slideWidth()
-					}, {
-						duration: s.speed,
-						queue: false
-					});
-				}
-			}
-			
-			var progress = function (action) {
-				$('.progress .bar').stop().width('0%');
-				if(action != 'pause') {
-					$('.progress .bar').animate({
-						width: '100%'
-					}, {
-						duration: s.delay, 
-						easing: 'linear',
-						queue: false
-					});
-				}
 			}
 		},	// init method
+		autoRotate : function (action) {
+			var s = methods.s;
+			if(action == 'pause') {
+				clearInterval(auto);
+			} else {
+				auto = window.setInterval( function() {
+					if (s.carousel) {
+						methods.shiftCarousel('right');
+					} else {
+						methods.nextSlide('right');
+					}
+				}, s.delay);
+	  			if (s.progress) {
+					methods.progress();
+				}
+			}
+		},
+		animateProgress : function (action) {
+			$('.progress .bar').stop().width('0%');
+			if(action != 'pause') {
+				$('.progress .bar').animate({
+					width: '100%'
+				}, {
+					duration: s.delay, 
+					easing: 'linear',
+					queue: false
+				});
+			}
+		},
+		shiftCarousel : function (direction) {
+			var cw = s.slideCount() * s.slideWidth(),
+				co = Math.abs(parseFloat(s.container.find(s.slideHolder).css('left'))),
+				cv = parseFloat(s.container.width()),
+				cp = parseFloat(s.container.css('paddingLeft'));
+			if((s.container.find(s.slideHolder + ':animated').length == 0) && 
+			((direction == 'left' && co != cp) || 
+			(direction == 'right' && (co + cv + cp) < cw))) {
+				s.container.find(s.slideHolder).animate({
+					left: ((direction == 'left') ? '+=' : '-=') + s.slideWidth()
+				}, {
+					duration: s.speed,
+					queue: false
+				});
+			}
+		},
+		nextSlide : function (direction) {
+			var s = methods.s, currentSlide = s.container.find(s.slideHolder + ' > ' + s.slide + '.active').index(), nextSlide, animateFrom, animateTo;
+			s.isAnimating = 1;
+			if(typeof direction === 'number') {
+				nextSlide = direction;
+				direction = (nextSlide > currentSlide) ? 'right' : 'left';
+			}
+			if (direction == 'right') {
+				if (typeof nextSlide !== 'number') {
+					nextSlide = (currentSlide == (s.slideCount() - 1)) ? 0 : currentSlide + 1;
+				}
+				animateFrom = s.slideWidth();
+				animateTo = '-=' + s.slideWidth();
+			} else {
+				if (typeof nextSlide !== 'number') {
+					nextSlide = (currentSlide == 0) ? s.slideCount() - 1 : currentSlide - 1;
+				}
+				animateFrom = -s.slideWidth();
+				animateTo = '+=' + s.slideWidth();
+			}
+			// Animate next slide
+			s.container.find(s.slideHolder + ' > ' + s.slide +  ':eq(' + nextSlide + ')')
+			  .css('left',animateFrom)
+			  .show()
+			  .animate({
+			  		left: animateTo
+			    }, s.speed, 
+			    function() {
+				    $(this).addClass('active');
+				    s.isAnimating = 0;
+				    s.container.find(s.slideNav + ' ' + s.navBtns.slide + ":eq(" + nextSlide + ")").addClass('active').siblings().removeClass('active');
+			  });
+			// Animate active slide
+			s.container.find(s.slideHolder + ' > ' + s.slide + '.active').animate({
+			    left: animateTo
+			  }, s.speed, function() {
+			    $(this).removeClass('active').hide();
+				s.container.trigger('activeSlide');
+			});
+	  		if (s.progress) {
+				$('.progress .bar').stop();
+		  		progress();
+			}
+		},
 		currentSlide : function () {
 			var s = $(this).data('s');
 			return s.container.find(s.slideHolder + ' > ' + s.slide + '.active').index() + 1;
 		},
 		navCheck : function() {
-			var s = $(this).data('s');
+			var s = methods.s;
 			// Hide nav when only one image or nav is disabled
 			if (s.slideCount() == 1 || s.nav == false) {
 				s.container.find(s.arrowsWrap + ',' + s.slideNav).hide();
@@ -209,6 +153,53 @@
 				for(var i = 0; i < s.slideCount(); i++) {
 					s.container.find(s.slideNav).append("<" + s.navBtns.slide + " data-slide-id='" + i + "' href='#'>" + i + "</" + s.navBtns.slide + ">");
 				}
+			}
+		},
+		bindEvents : function () {
+			var s = methods.s;
+			s.container.find(s.navBtns.right).click(function(e){
+				if(!s.isAnimating) {
+					e.preventDefault();
+					if (s.carousel == true) {
+						methods.shiftCarousel('right');
+					} else {
+						methods.nextSlide('right');
+					}
+					if (s.auto) {
+						clearInterval(auto);
+						methods.autoRotate();
+					}
+				}
+			});
+			s.container.find(s.navBtns.left).click(function(e){
+				if(!s.isAnimating) {
+					e.preventDefault();
+					if (s.carousel == true) {
+						methods.shiftCarousel('left');
+					} else {
+						methods.nextSlide('left');
+					}
+					if (s.auto) {
+						clearInterval(auto);
+						methods.autoRotate();
+					}
+				}
+			});
+			s.container.find(s.slideNav).children().click(function(e){
+				if(!s.isAnimating && !$(this).hasClass('active')) {
+					e.preventDefault();
+					methods.nextSlide(parseInt($(this).attr('data-slide-id')));
+				}
+			});
+			if (s.pause) {
+  				s.container.find(s.slide).hover(function(){ 
+  					methods.autoRotate('pause');
+  					if (s.progress) {
+  						methods.progress('pause');
+  					}
+  				}, function() {
+  					methods.autoRotate();
+  				});
 			}
 		}
 
